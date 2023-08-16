1f207fa08ce8752115c004b7564f9b0dd6547ab3
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-08-16 17:06:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  return key in sampleObject;
 }
 
 // Do not change the code below
